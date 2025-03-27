c8430fbcd130046a66fcfcc454162e9665beab7b
Saurabh Rathod <saurabh.r@prometteur.in>
2025-03-27 12:04:05 +0530

    convert the question plen object into class instances

--- script/question.js
+++ script/question.js
@@ -1,123 +1,136 @@
-
-export const questions = [
-  {
-    id: "1",
-    questionText: "What does `var` mean in JavaScript?",
-    options: [
-      "Variable",
-      "Value",
-      "Function",
-      "None of the above"
-    ],
-    correctAnswer: "Variable",
-    keywords: ["JavaScript", "variables"]
-  },
-  {
-    id: "2",
-    questionText: "Which of the following is used to define a function in JavaScript?",
-    options: [
-      "def",
-      "function",
-      "func",
-      "define"
-    ],
-    correctAnswer: "function",
-    keywords: ["JavaScript", "functions"]
-  },
-  {
-    id: "3",
-    questionText: "What is the output of `typeof null` in JavaScript?",
-    options: [
-      "null",
-      "object",
-      "undefined",
-      "NaN"
-    ],
-    correctAnswer: "object",
-    keywords: ["JavaScript", "data types", "null"]
-  },
-  {
-    id: "4",
-    questionText: "Which method is used to convert a string to an integer in JavaScript?",
-    options: [
-      "parseInt()",
-      "parseString()",
-      "int()",
-      "toString()"
-    ],
-    correctAnswer: "parseInt()",
-    keywords: ["JavaScript", "functions", "parse"]
-  },
-  {
-    id: "5",
-    questionText: "What will the following code log to the console: `console.log(1 + '1')`?",
-    options: [
-      "'11'",
-      "2",
-      "11",
-      "Error"
-    ],
-    correctAnswer: "'11'",
-    keywords: ["JavaScript", "strings", "type coercion"]
-  },
-  {
-    id: "6",
-    questionText: "What is the correct syntax to create an object in JavaScript?",
-    options: [
-      "let obj = {}",
-      "let obj = []",
-      "let obj = ()",
-      "let obj = ''"
-    ],
-    correctAnswer: "let obj = {}",
-    keywords: ["JavaScript", "objects"]
-  },
-  {
-    id: "7",
-    questionText: "Which of the following is NOT a JavaScript data type?",
-    options: [
-      "String",
-      "Boolean",
-      "Object",
-      "Float"
-    ],
-    correctAnswer: "Float",
-    keywords: ["JavaScript", "data types"]
-  },
-  {
-    id: "8",
-    questionText: "How do you write a comment in JavaScript?",
-    options: [
-      "# This is a comment",
-      "// This is a comment",
-      "<!-- This is a comment -->",
-      "/* This is a comment */"
-    ],
-    correctAnswer: "// This is a comment",
-    keywords: ["JavaScript", "comments"]
-  },
-  {
-    id: "9",
-    questionText: "What is the value of `x` after the following code: `let x = 10; x += 5;`?",
-    options: [
-      "5",
-      "15",
-      "10",
-      "None of the above"
-    ],
-    correctAnswer: "15",
-    keywords: ["JavaScript", "operators"]
-  },
-  {
-    id: "10",
-    questionText: "Which operator is used to compare both value and type in JavaScript?",
-    options: [
-      "==",
-      "===",
-      "=",
-      "!="
-    ],
-    correctAnswer: "===",
-    keywords: ["JavaScript", "operators"]
+export class Question {
+  constructor(questionText, options, correctAnswer) {
+    this.questionText = questionText;
+    this.options = options;
+    this.correctAnswer = correctAnswer;
+    this.selectedAnswer = null; 
   }
-];
+
+  checkAnswer() {
+    return this.selectedAnswer === this.correctAnswer;
+  }
+}
+
+
+// export const questions = [
+//   {
+//     id: "1",
+//     questionText: "What does `var` mean in JavaScript?",
+//     options: [
+//       "Variable",
+//       "Value",
+//       "Function",
+//       "None of the above"
+//     ],
+//     correctAnswer: "Variable",
+//     keywords: ["JavaScript", "variables"]
+//   },
+//   {
+//     id: "2",
+//     questionText: "Which of the following is used to define a function in JavaScript?",
+//     options: [
+//       "def",
+//       "function",
+//       "func",
+//       "define"
+//     ],
+//     correctAnswer: "function",
+//     keywords: ["JavaScript", "functions"]
+//   },
+//   {
+//     id: "3",
+//     questionText: "What is the output of `typeof null` in JavaScript?",
+//     options: [
+//       "null",
+//       "object",
+//       "undefined",
+//       "NaN"
+//     ],
+//     correctAnswer: "object",
+//     keywords: ["JavaScript", "data types", "null"]
+//   },
+//   {
+//     id: "4",
+//     questionText: "Which method is used to convert a string to an integer in JavaScript?",
+//     options: [
+//       "parseInt()",
+//       "parseString()",
+//       "int()",
+//       "toString()"
+//     ],
+//     correctAnswer: "parseInt()",
+//     keywords: ["JavaScript", "functions", "parse"]
+//   },
+//   {
+//     id: "5",
+//     questionText: "What will the following code log to the console: `console.log(1 + '1')`?",
+//     options: [
+//       "'11'",
+//       "2",
+//       "11",
+//       "Error"
+//     ],
+//     correctAnswer: "'11'",
+//     keywords: ["JavaScript", "strings", "type coercion"]
+//   },
+//   {
+//     id: "6",
+//     questionText: "What is the correct syntax to create an object in JavaScript?",
+//     options: [
+//       "let obj = {}",
+//       "let obj = []",
+//       "let obj = ()",
+//       "let obj = ''"
+//     ],
+//     correctAnswer: "let obj = {}",
+//     keywords: ["JavaScript", "objects"]
+//   },
+//   {
+//     id: "7",
+//     questionText: "Which of the following is NOT a JavaScript data type?",
+//     options: [
+//       "String",
+//       "Boolean",
+//       "Object",
+//       "Float"
+//     ],
+//     correctAnswer: "Float",
+//     keywords: ["JavaScript", "data types"]
+//   },
+//   {
+//     id: "8",
+//     questionText: "How do you write a comment in JavaScript?",
+//     options: [
+//       "# This is a comment",
+//       "// This is a comment",
+//       "<!-- This is a comment -->",
+//       "/* This is a comment */"
+//     ],
+//     correctAnswer: "// This is a comment",
+//     keywords: ["JavaScript", "comments"]
+//   },
+//   {
+//     id: "9",
+//     questionText: "What is the value of `x` after the following code: `let x = 10; x += 5;`?",
+//     options: [
+//       "5",
+//       "15",
+//       "10",
+//       "None of the above"
+//     ],
+//     correctAnswer: "15",
+//     keywords: ["JavaScript", "operators"]
+//   },
+//   {
+//     id: "10",
+//     questionText: "Which operator is used to compare both value and type in JavaScript?",
+//     options: [
+//       "==",
+//       "===",
+//       "=",
+//       "!="
+//     ],
+//     correctAnswer: "===",
+//     keywords: ["JavaScript", "operators"]
+//   }
+// ];
--- script/quiz.js
+++ script/quiz.js
@@ -1,4 +1,18 @@
-import { questions } from "./question.js";
+import {Question} from "./question.js";
+
+
+export const questions = [
+  new Question("What does `var` mean in JavaScript?", ["Variable", "Value", "Function", "None of the above"], "Variable"),
+  new Question("Which of the following is used to define a function in JavaScript?", ["def", "function", "func", "define"], "function"),
+  new Question("What is the output of `typeof null` in JavaScript?", ["null", "object", "undefined", "NaN"], "object"),
+  new Question("Which method is used to convert a string to an integer in JavaScript?", ["parseInt()", "parseString()", "int()", "toString()"], "parseInt()"),
+  new Question("What will the following code log to the console: `console.log(1 + '1')`?", ["'11'", "2", "11", "Error"], "'11'"),
+  new Question("What is the correct syntax to create an object in JavaScript?", ["let obj = {}", "let obj = []", "let obj = ()", "let obj = ''"], "let obj = {}"),
+  new Question("Which of the following is NOT a JavaScript data type?", ["String", "Boolean", "Object", "Float"], "Float"),
+  new Question("How do you write a comment in JavaScript?", ["# This is a comment", "// This is a comment", "<!-- This is a comment -->", "/* This is a comment */"], "// This is a comment"),
+  new Question("What is the value of `x` after the following code: `let x = 10; x += 5;`?", ["5", "15", "10", "None of the above"], "15"),
+  new Question("Which operator is used to compare both value and type in JavaScript?", ["==", "===", "=", "!="], "===")
+];
 
 class Quiz {
   constructor(questions) {
@@ -23,6 +37,11 @@ class Quiz {
     });
 
     document.getElementById('current-question-number').textContent = this.currentQuestionIndex + 1;
+
+
+    // document.getElementById('next-button').disabled = currentQuestion.selectedAnswer === null;
+
+    // document.getElementById('previous-button').disabled = currentQuestion.selectedAnswer === null;
   }
 
   handleAnswerSelection(selectedOption, selectedElement) {
@@ -37,9 +56,21 @@ class Quiz {
     // Add the 'selected' class to the newly selected option
     selectedElement.classList.add('selected');
     currentQuestion.selectedAnswer = selectedOption;
+
+    // document.getElementById('next-button').disabled = false;
+    // document.getElementById('previous-button').disabled = false;
+    
   }
 
   nextQuestion() {
+
+    // const currentQuestion = this.questions[this.currentQuestionIndex];
+
+    // if(currentQuestion.selectedAnswer === null){
+    //   alert('Please select an option before moving to the next question.');
+    //   return;
+    // }
+
     if (this.currentQuestionIndex < this.totalQuestions - 1) {
       this.currentQuestionIndex++;
       this.loadQuestion();
@@ -48,6 +79,12 @@ class Quiz {
     }
   }
   previousQuestion() {
+    // const currentQuestion = this.questions[this.currentQuestionIndex];
+
+    // if(currentQuestion.selectedAnswer === null){
+    //   alert('Please select an option before moving to the next question.');
+    //   return;
+    // }
     if (this.currentQuestionIndex < this.totalQuestions - 1) {
       this.currentQuestionIndex--;
       this.loadQuestion();
@@ -71,6 +108,7 @@ class Quiz {
     document.getElementById('question-container').classList.add('hidden');
     document.getElementById('question-progress').classList.add('hidden');
     document.getElementById('next-button').classList.add('hidden');
+    document.getElementById('previous-button').classList.add('hidden');
     document.getElementById('result-container').classList.remove('hidden');
 
     // Calculate score (correct answers count)
